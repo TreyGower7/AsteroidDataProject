53cab9979c40a266e543db38650bfcdccabfd282
Trey Gower <goweryert@gmail.com>
2023-04-24 00:12:07 +0000

    now i fixed merge issues

--- docker-src/src/asteroid_data.py
+++ docker-src/src/asteroid_data.py
@@ -178,81 +178,6 @@ def spec_ast(ast_name: str) -> dict:
     except TypeError:
         return f'invalid asteroid name or no data found with error\n'
 
-@app.route('/image', methods=['POST'])
-def image() -> str:
-    """
-    Takes user input and generates a graph based on the data the user wants plotted
-    args:
-        a string specifying a key to access data corresponding to that key
-    returns:
-        a string ensuring the graph was made or deleted
-    """   
-    try:
-        plot_data = json.loads(rd.get('ast_data'))  
-        lists = []
-        sorted_data = sorted(plot_data, key=lambda x: float(x['moid_ld']))
-        for x in range(len(sorted_data)):
-    	    lists.append(float(sorted_data[x]['moid_ld']))
-        try:
-            limit = int(request.args.get('farthest', 1000)) 
-            start = int(request.args.get('closest', 0))
-            if start < 300 or start > 1000 or limit < 300 or limit > 1000: 
-                return "There are no asteroids closer than 300 AU or farther than 1000 AU\n"
-            if limit < start: 
-                return "The farthest value is lower than the closest value, this can't happen try again with different values\n" 
-            result = []
-            for x in range(len(lists)):
-                if lists[x] >= limit: 
-                    break
-                elif lists[x] >= start:
-                    result.append(lists[x]) 
-        except ValueError: 
-            return "Make sure the number closest and farthest values are numbers\n"  
-        split = (limit - start)/5 
-        range1 = [start,start+split] 
-        range2 = [start+split, start+(split+split)] 
-        range3 = [start+(split+split),start+(split+split+split)]
-        range4 = [start+(split+split+split), start+(split+split+split+split)]  
-        range5 = [start+(split+split+split+split),limit] 
-    
-        count1 = len([i for i in result if range1[0] <= i < range1[1]])
-        count2 = len([i for i in result if range2[0] <= i < range2[1]])
-        count3 = len([i for i in result if range3[0] <= i < range3[1]])
-        count4 = len([i for i in result if range4[0] <= i <= range4[1]])
-        count5 = len([i for i in result if range5[0] <= i <= range5[1]])
-    # Creating the bar graph
-    
-        fig, ax = plt.subplots()
-        ax.bar([1,2,3,4,5],[count1, count2, count3, count4, count5])
-        ax.set_xticks([1, 2, 3, 4, 5])
-        ax.set_xticklabels([f'{start}-{start+(split)}', f'{start+(split)}-{start+(split+split)}', f'{start+(split+split)}-{start+(split+split+split)}', f'{start+(split+split+split)}-{start+(split+split+split+split)}', f'{start+(split+split+split+split)}-{limit}'])
-        ax.set_xlabel('Distance from Earth (AU)')
-        ax.set_ylabel('Number of Asteroids') 
-        ax.set_title('Number of Asteroids a certain Distance from earth') 
-        plt.savefig('asteroid_graph.png')
-        file_bytes = open('./asteroid_graph.png', 'rb').read()
-        rd2.set('key', file_bytes)
-        return "Image is posted\n" 
-    except TypeError: 
-        return "Make sure the data has been posted\n" 
-    except NameError:
-        return "Make sure the data has been posted\n"
-
-@app.route('/image', methods=['GET'])
-def get_image() -> str:
-        try:
-            path = './asteroid_graph.png'
-            with open(path, 'wb') as f: 
-                f.write(rd2.get('key'))
-            return send_file(path, mimetype='image/png', as_attachment=True) 
-        except TypeError:
-            return "Post the data first and then post the image to use this function\n"
-
-@app.route('/image', methods=['DELETE'])
-def del_image() -> str: 
-        rd2.delete('key') 
-        return "Graph was deleted\n"
-
 @app.route('/<string:ast_name>/temp', methods=['GET'])
 def temp(ast_name: str) -> dict:
     """
@@ -266,13 +191,13 @@ def temp(ast_name: str) -> dict:
     try:
         asteroid = spec_ast(ast_name)
         s_knot = 1376 #w/m^2
-        albedo = float(asteroid['albedo'])  
-        boltzman = 5.67 * (10**-8) 
-        temp = float(((s_knot*(1-albedo))/(4*boltzman)) ** (1/4))   
-        awnser = {'Temp': temp, 'units': "Kelvin"} 
-        return awnser 
-    except TypeError: 
-        return "Make sure asteroid name is correct\n" 
+        albedo = float(asteroid['albedo'])
+        boltzman = 5.67 * (10**-8)
+        temp = float(((s_knot*(1-albedo))/(4*boltzman)) ** (1/4))
+        awnser = {'Temp': temp, 'units': "Kelvin"}
+        return awnser
+    except TypeError:
+        return "Make sure asteroid name is correct\n"
 
 @app.route('/<string:ast_name>/luminosity', methods=['GET'])
 def lumin(ast_name: str) -> str:
